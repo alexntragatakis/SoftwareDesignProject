f08d8e17e5cdfff922e99e438be371ff6015ddd8
jalee <jalee4630@gmail.com>
2025-12-01 14:31:08 -0500

    Menu background image

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include "FEHLCD.h"
+#include <FEHImages.h>
 
 int main()
 {
-    LCD.WriteLine("Hello World!");
+    /* Draw background image for menu screen */
+    FEHImage menuBg;
+    menuBg.Open("MenuBG.png");
+    menuBg.Draw(0, -25);
+    
+
     while (1) {
         LCD.Update();
         // Never end
