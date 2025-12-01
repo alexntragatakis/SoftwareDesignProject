01aca5681df50f698ff216ca4b034f565a24218c
jalee <jalee4630@gmail.com>
2025-12-01 15:07:22 -0500

    Added buttons for menu (no touch code)

--- main.cpp
+++ main.cpp
@@ -3,11 +3,29 @@
 
 int main()
 {
-    /* Draw background image for menu screen */
     FEHImage menuBg;
+
+    /* Background image for menu screen */
     menuBg.Open("MenuBG.png");
-    menuBg.Draw(0, -25);
+    menuBg.Draw(0, -10);
     
+    /* Buttons on menu */
+    LCD.SetFontColor(LIGHTBLUE);
+    LCD.FillRectangle(40, 115, 100, 40);
+    LCD.FillRectangle(175, 115, 100, 40);
+    LCD.FillRectangle(40, 175, 100, 40);
+    LCD.FillRectangle(175, 175, 100, 40);
+    LCD.SetFontColor(WHITESMOKE);
+    LCD.DrawRectangle(40, 115, 100, 40);
+    LCD.DrawRectangle(175, 115, 100, 40);
+    LCD.DrawRectangle(40, 175, 100, 40);
+    LCD.DrawRectangle(175, 175, 100, 40);
+    LCD.SetFontColor(BLACK);
+    LCD.SetFontScale(0.5);
+    LCD.WriteAt("Play", 78, 128);
+    LCD.WriteAt("How to Play", 192, 128);
+    LCD.WriteAt("Leaderboard", 55, 188);
+    LCD.WriteAt("Credits", 203, 188);
 
     while (1) {
         LCD.Update();
